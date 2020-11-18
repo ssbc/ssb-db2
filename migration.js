838c28728eecaf84159fb7eab2192ed22b688e9a
Andre Staltz <andre@staltz.com>
2020-11-18 18:05:22 +0200

    emit migration progress events

--- migration.js
+++ migration.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const pull = require('pull-stream')
+const Notify = require('pull-notify')
 const FlumeLog = require('flumelog-offset')
 const bipf = require('bipf')
 const jsonCodec = require('flumecodec/json')
@@ -7,28 +8,40 @@ const debug = require('debug')('ssb:db2:migration')
 
 const blockSize = 64 * 1024
 
-function skip(count) {
+function skip(count, onDone) {
   let skipped = 0
-  return pull.filter(() => {
+  return pull.filter((x) => {
     if (skipped >= count) return true
     else {
       skipped++
+      if (skipped === count && onDone) onDone(x)
       return false
     }
   })
 }
 
-function getOldLogStream(sbot, config, live) {
+function getOldLogStreams(sbot, config, live) {
   const old = !live
-  if (sbot.createRawLogStream) {
-    return pull(
-      sbot.createRawLogStream({ old, live }),
-      pull.map((obj) => obj.value)
+  if (sbot.createRawLogStream && sbot.createSequenceStream) {
+    const logStream = sbot.createRawLogStream({ old, live })
+    if (live) return [logStream]
+    const sizeStream = pull(
+      sbot.createSequenceStream(),
+      pull.filter((x) => x >= 0)
     )
+    return [logStream, sizeStream]
   } else {
     const oldLogPath = path.join(config.path, 'flume', 'log.offset')
     const oldLog = FlumeLog(oldLogPath, { blockSize, codec: jsonCodec })
-    return pull(oldLog.stream({ seqs: false, old, live, codec: jsonCodec }))
+    const logStream = oldLog.stream({ seqs: true, old, live, codec: jsonCodec })
+    if (live) return [logStream]
+    const notify = Notify()
+    oldLog.since(notify)
+    const sizeStream = pull(
+      notify.listen(),
+      pull.filter((x) => x >= 0)
+    )
+    return [logStream, sizeStream]
   }
 }
 
@@ -55,9 +68,37 @@ function scanAndCount(pushstream, cb) {
 }
 
 exports.init = function init(sbot, config) {
-  const oldLogStream = getOldLogStream(sbot, config, false)
-  const oldLogStreamLive = getOldLogStream(sbot, config, true)
+  const [oldLogStream, oldSizeStream] = getOldLogStreams(sbot, config, false)
+  const [oldLogStreamLive] = getOldLogStreams(sbot, config, true)
   const newLogStream = sbot.db.log.stream({ gte: 0 })
+
+  let oldSize = null
+  let migratedSize = null
+
+  function updateOldSize(read) {
+    read(null, function next(end, data) {
+      if (end === true) return
+      if (end) throw end
+      oldSize = data
+      read(null, next)
+    })
+  }
+
+  function updateMigratedSize(obj) {
+    migratedSize = obj.seq
+  }
+
+  function updateMigratedSizeAndPluck(obj) {
+    updateMigratedSize(obj)
+    return obj.value
+  }
+
+  function emitProgressEvent() {
+    if (oldSize !== null && migratedSize !== null) {
+      const progress = migratedSize / oldSize
+      sbot.emit('ssb:db2:migration:progress', progress)
+    }
+  }
 
   let dataTransferred = 0 // FIXME: does this only work if the new log is empty?
   function writeTo(log) {
@@ -66,18 +107,26 @@ exports.init = function init(sbot, config) {
       // FIXME: could we use log.add since it already converts to BIPF?
       // FIXME: see also issue #16
       log.append(data, () => {})
+      emitProgressEvent()
       if (dataTransferred % blockSize == 0) log.onDrain(cb)
       else cb()
     }
   }
 
+  updateOldSize(oldSizeStream)
+
   scanAndCount(newLogStream, (err, msgCountNewLog) => {
+    if (err) return console.error(err)
     if (msgCountNewLog === 0) debug('new log is empty, will start migrating')
     else debug('new log has %s msgs, will continue migrating', msgCountNewLog)
 
     pull(
       oldLogStream,
-      skip(msgCountNewLog),
+      skip(msgCountNewLog, function whenDoneSkipping(obj) {
+        updateMigratedSize(obj)
+        emitProgressEvent()
+      }),
+      pull.map(updateMigratedSizeAndPluck),
       pull.map(toBIPF),
       pull.asyncMap(writeTo(sbot.db.log)),
       pull.reduce(
@@ -89,6 +138,7 @@ exports.init = function init(sbot, config) {
 
           pull(
             oldLogStreamLive,
+            pull.map(updateMigratedSizeAndPluck),
             pull.map(toBIPF),
             pull.asyncMap(writeTo(sbot.db.log)),
             pull.drain(() => {
--- test/migration.js
+++ test/migration.js
@@ -7,6 +7,8 @@ const SecretStack = require('secret-stack')
 const caps = require('ssb-caps')
 const generateFixture = require('ssb-fixtures')
 const fs = require('fs')
+const pull = require('pull-stream')
+const fromEvent = require('pull-stream-util/from-event')
 const { toCallback } = require('../operators')
 
 const dir = '/tmp/ssb-db2-migration'
@@ -14,11 +16,13 @@ const dir = '/tmp/ssb-db2-migration'
 rimraf.sync(dir)
 mkdirp.sync(dir)
 
+const TOTAL = 10
+
 test('generate fixture with flumelog-offset', (t) => {
   generateFixture({
     outputDir: dir,
     seed: 'migration',
-    messages: 2,
+    messages: TOTAL,
     authors: 5,
     slim: true,
   }).then(() => {
@@ -37,21 +41,43 @@ test('migration moves msgs from old log to new log', (t) => {
     .use(require('../migration'))
     .call(null, { keys, path: dir })
 
-  // FIXME: replace setTimeout with migration progress events
-  setTimeout(() => {
-    t.true(fs.existsSync(path.join(dir, 'db2', 'log.bipf')), 'migration done')
-    sbot.db.onDrain(() => {
-      sbot.db.query(
-        toCallback((err1, msgs) => {
-          t.error(err1, 'no err')
-          t.equal(msgs.length, 2)
-          sbot.close(() => {
-            t.end()
-          })
-        })
-      )
+  let progressEventsReceived = false
+  pull(
+    fromEvent('ssb:db2:migration:progress', sbot),
+    pull.take(TOTAL),
+    pull.collect((err, nums) => {
+      t.error(err)
+      t.equals(nums.length, TOTAL, `${TOTAL} progress events emitted`)
+      t.equals(nums[0], 0, 'first progress event is zero')
+      t.true(nums[0] < nums[1], 'monotonically increasing')
+      t.true(nums[1] < nums[2], 'monotonically increasing')
+      t.equals(nums[TOTAL - 1], 1, 'last progress event is one')
+      progressEventsReceived = true
     })
-  }, 1000)
+  )
+
+  pull(
+    fromEvent('ssb:db2:migration:progress', sbot),
+    pull.filter((x) => x === 1),
+    pull.take(1),
+    // FIXME: why do we still need a setTimeout?
+    pull.asyncMap((x, cb) => setTimeout(cb, 500)),
+    pull.drain(() => {
+      t.true(fs.existsSync(path.join(dir, 'db2', 'log.bipf')), 'migration done')
+      sbot.db.onDrain(() => {
+        sbot.db.query(
+          toCallback((err1, msgs) => {
+            t.error(err1, 'no err')
+            t.equal(msgs.length, TOTAL)
+            t.true(progressEventsReceived, 'progress events received')
+            sbot.close(() => {
+              t.end()
+            })
+          })
+        )
+      })
+    })
+  )
 })
 
 test('migration keeps new log synced with old log being updated', (t) => {
@@ -62,34 +88,50 @@ test('migration keeps new log synced with old log being updated', (t) => {
     .use(require('../migration'))
     .call(null, { keys, path: dir })
 
-  // FIXME: replace setTimeout with migration progress events
-  setTimeout(() => {
-    t.true(fs.existsSync(path.join(dir, 'db2', 'log.bipf')), 'migration done')
-    sbot.db.onDrain(() => {
-      sbot.db.query(
-        toCallback((err1, msgs) => {
-          t.error(err1, '1st query suceeded')
-          t.equal(msgs.length, 2, '2 msgs')
+  pull(
+    fromEvent('ssb:db2:migration:progress', sbot),
+    pull.filter((x) => x === 1),
+    pull.take(1),
+    // FIXME: why do we still need a setTimeout?
+    pull.asyncMap((x, cb) => setTimeout(cb, 500)),
+    pull.drain(() => {
+      t.true(fs.existsSync(path.join(dir, 'db2', 'log.bipf')), 'migration done')
+      sbot.db.onDrain(() => {
+        sbot.db.query(
+          toCallback((err1, msgs) => {
+            t.error(err1, '1st query suceeded')
+            t.equal(msgs.length, TOTAL, `${TOTAL} msgs`)
 
-          sbot.publish({ type: 'post', text: 'Extra post' }, (err2, posted) => {
-            t.error(err2, 'publish suceeded')
-            t.equals(posted.value.content.type, 'post', 'msg posted')
-
-            // FIXME: replace setTimeout with migration progress events
-            setTimeout(() => {
-              sbot.db.query(
-                toCallback((err3, msgs2) => {
-                  t.error(err3, '2nd query suceeded')
-                  t.equal(msgs2.length, 3, '3 msgs')
-                  sbot.close(() => {
-                    t.end()
+            // This should run after the sbot.publish completes
+            pull(
+              fromEvent('ssb:db2:migration:progress', sbot),
+              pull.filter((x) => x === 1),
+              pull.take(1),
+              // FIXME: why do we still need a setTimeout?
+              pull.asyncMap((x, cb) => setTimeout(cb, 500)),
+              pull.drain(() => {
+                sbot.db.query(
+                  toCallback((err3, msgs2) => {
+                    t.error(err3, '2nd query suceeded')
+                    t.equal(msgs2.length, TOTAL + 1, `${TOTAL + 1} msgs`)
+                    sbot.close(() => {
+                      t.end()
+                    })
                   })
-                })
-              )
-            }, 1000)
+                )
+              })
+            )
+
+            sbot.publish(
+              { type: 'post', text: 'Extra post' },
+              (err2, posted) => {
+                t.error(err2, 'publish suceeded')
+                t.equals(posted.value.content.type, 'post', 'msg posted')
+              }
+            )
           })
-        })
-      )
+        )
+      })
     })
-  }, 1000)
+  )
 })
