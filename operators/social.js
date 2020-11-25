8f3c5db74d856f8ab31a7175ee0195e8ae03c019
Anders Rune Jensen <arj03@protonmail.ch>
2020-11-25 13:07:00 +0100

    Support live queries in social index

--- operators/social.js
+++ operators/social.js
@@ -2,19 +2,19 @@ const { deferred } = require('jitdb/operators')
 
 function hasRoot(msgKey) {
   return deferred((meta, cb) => {
-    meta.db2.indexes.social.getMessagesByRoot(msgKey, cb)
+    meta.db2.indexes.social.getMessagesByRoot(msgKey, meta.live, cb)
   })
 }
 
 function votesFor(msgKey) {
   return deferred((meta, cb) => {
-    meta.db2.indexes.social.getMessagesByVoteLink(msgKey, cb)
+    meta.db2.indexes.social.getMessagesByVoteLink(msgKey, meta.live, cb)
   })
 }
 
 function mentions(key) {
   return deferred((meta, cb) => {
-    meta.db2.indexes.social.getMessagesByMention(key, cb)
+    meta.db2.indexes.social.getMessagesByMention(key, meta.live, cb)
   })
 }
 
