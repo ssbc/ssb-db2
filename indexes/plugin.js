0d22e5496301fca6b039e09a69ca17f22e142521
Anders Rune Jensen <arj03@protonmail.ch>
2020-12-25 20:15:29 +0100

    Add comment

--- indexes/plugin.js
+++ indexes/plugin.js
@@ -54,7 +54,7 @@ module.exports = function (
   function onData(record, isLive) {
     // maybe check if for us!
     let unwritten = handleData(record, processed)
-    if (unwritten > 0) unWrittenOffset = record.seq
+    if (unwritten > 0) unWrittenOffset = record.seq // "seq" is abstract, here means "offset"
     processed++
 
     if (unwritten > chunkSize || isLive) writeBatch(() => {})
