5018eb7426074fbc43de2203dcfc88c718a65d7e
Andre Staltz <andre@staltz.com>
2021-01-27 16:15:11 +0200

    expose extra operator fullMentions

--- full-mentions.js
+++ full-mentions.js
@@ -1,5 +1,7 @@
 const FullMentionsIndex = require('./indexes/full-mentions')
+const fullMentions = require('./operators/full-mentions')
 
 exports.init = function (sbot, config) {
   sbot.db.registerIndex(FullMentionsIndex)
+  sbot.db.operators.fullMentions = fullMentions
 }
