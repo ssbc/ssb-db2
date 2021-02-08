f79ae6cd0a20ddd61fb88e755224bb3fc98a9198
Andre Staltz <andre@staltz.com>
2021-02-08 15:48:14 +0200

    add manifest in compat/db.js

--- compat/db.js
+++ compat/db.js
@@ -1,3 +1,10 @@
+// exports.name is blank to merge into global namespace
+
+exports.manifest = {
+  publish: 'async',
+  whoami: 'sync',
+}
+
 exports.init = function (sbot, config) {
   sbot.publish = sbot.db.publish
   sbot.whoami = () => ({ id: sbot.id })
