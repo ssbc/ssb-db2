4948919dab9e7a8380840cd1d3a3eb587b09905f
Andre Staltz <andre@staltz.com>
2021-02-15 15:37:14 +0200

    implement createWriteStream in compat/db

--- compat/db.js
+++ compat/db.js
@@ -1,12 +1,27 @@
+const pull = require('pull-stream')
+
 // exports.name is blank to merge into global namespace
 
 exports.manifest = {
   publish: 'async',
   whoami: 'sync',
+  createWriteStream: 'sink',
 }
 
 exports.init = function (sbot, config) {
   sbot.publish = sbot.db.publish
   sbot.whoami = () => ({ id: sbot.id })
   sbot.ready = () => true
+  sbot.createWriteStream = function createWriteStream(cb) {
+    return pull(
+      pull.asyncMap(sbot.db.add),
+      pull.drain(
+        () => {},
+        cb ||
+          ((err) => {
+            if (err) throw err
+          })
+      )
+    )
+  }
 }
