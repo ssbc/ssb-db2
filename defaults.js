fc350d5e4f6c7a5f52fef8782e8735af8bddf63b
Andre Staltz <andre@staltz.com>
2021-02-18 13:43:56 +0200

    use pull-drain-gently in migrate

--- defaults.js
+++ defaults.js
@@ -4,3 +4,11 @@ exports.BLOCK_SIZE = 64 * 1024
 exports.oldLogPath = (dir) => path.join(dir, 'flume', 'log.offset')
 exports.newLogPath = (dir) => path.join(dir, 'db2', 'log.bipf')
 exports.indexesPath = (dir) => path.join(dir, 'db2', 'indexes')
+exports.tooHotOpts = (config) =>
+  config.db2
+    ? {
+        ceiling: config.db2.maxCpu || Infinity,
+        wait: config.db2.maxCpuWait || 90,
+        maxPause: config.db2.maxCpuMaxPause || 300,
+      }
+    : { ceiling: Infinity, wait: 90, maxPause: 300 }
