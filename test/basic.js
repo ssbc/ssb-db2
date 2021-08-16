7d0d4679c044c74415b14dae35193091058ef3c1
Andre Staltz <andre@staltz.com>
2021-08-16 16:07:18 +0300

    tweak tests for publishAs bendy butt

--- test/basic.js
+++ test/basic.js
@@ -267,7 +267,7 @@ test('publishAs bendy butt', (t) => {
   const mainKeys = ssbKeys.generate()
 
   const content = {
-    type: "metafeed/add",
+    type: "metafeed/add/existing",
     feedpurpose: "main",
     subfeed: mainKeys.id,
     metafeed: mfKeys.id,
@@ -298,7 +298,7 @@ test('publishAs bendy butt', (t) => {
 
     db.get(msg.key, (err, msg) => {
       t.equal(msg.sequence, 1, 'sequence ok')
-      t.equal(msg.content.type, 'metafeed/add')
+      t.equal(msg.content.type, 'metafeed/add/existing')
       sbot.close(t.end)
     })
   })
