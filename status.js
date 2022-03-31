3e23bdd57e2a6c1c66ff06a06e6b066ab8f33022
André Staltz <andre@staltz.com>
2022-03-31 21:41:55 +0300

    improve progress calculation in status.js (#322)
    * reset jitdb status when progress reaches 100%
    
    * update calculateProgress math in status
    
    * avoid updating leveldb index status in vain
    
    * fix calculateProgress() end result in status.js
    
    * fix timer logic in status.js
    
    * use jitdb 6.3.0
    
    * update tests for prepare()

--- status.js
+++ status.js
@@ -5,23 +5,63 @@
 const Obv = require('obz')
 
 module.exports = function Status(log, jitdb) {
-  const statsObj = {
+  const stats = {
     log: log.since.value || 0,
     jit: {},
     indexes: {},
+    progress: 0,
   }
   const obv = Obv()
-  obv.set(statsObj)
+  obv.set(stats)
   const EMIT_INTERVAL = 1000
   let i = 0
   let iTimer = 0
   let timer = null
 
+  function avgPercent(offsets, stats) {
+    if (offsets.length === 0) return 1 // assume 100% if there are no numbers
+    const logSize = Math.max(1, stats.log)
+    const N = offsets.length
+    return Math.min(
+      offsets
+        .map((offset) => Math.max(0, offset)) // avoid negative numbers
+        .map((offset) => offset / logSize) // this index's progress
+        .reduce((sum, x) => sum + x, 0) / N, // avg = (sum of all progress) / N
+      1 // never go above 1
+    )
+  }
+
+  function avgOffset(offsets, stats) {
+    const logSize = Math.max(1, stats.log)
+    return avgPercent(offsets, stats) * logSize
+  }
+
+  // Crunch stats numbers to produce one number for the "indexing" progress
+  function calculateProgress() {
+    const avgJITDBOffset = avgOffset(Object.values(stats.jit), stats)
+    const offsets = Object.values(stats.indexes).concat(avgJITDBOffset)
+    return avgPercent(offsets, stats)
+  }
+
   jitdb.status((jitStats) => {
     updateLog()
-    statsObj.jit = jitStats
-    obv.set(statsObj)
+    stats.jit = jitStats
+    update()
   })
+
+  function update() {
+    ++i
+    if (!timer) {
+      iTimer = i
+      emit()
+      setTimer()
+    }
+  }
+
+  function emit() {
+    stats.progress = calculateProgress()
+    obv.set(stats)
+  }
 
   function setTimer() {
     // Turn on
@@ -33,25 +73,21 @@ module.exports = function Status(log, jitdb) {
         i = iTimer = 0
       } else {
         iTimer = i
-        obv.set(statsObj)
+        emit()
       }
     }, EMIT_INTERVAL)
     if (timer.unref) timer.unref()
   }
 
   function updateLog() {
-    statsObj.log = log.since.value || 0
+    stats.log = log.since.value || 0
   }
 
   function updateIndex(name, offset) {
+    if (stats.indexes[name] === offset) return
     updateLog()
-    statsObj.indexes[name] = offset
-    ++i
-    if (!timer) {
-      iTimer = i
-      obv.set(statsObj)
-      setTimer()
-    }
+    stats.indexes[name] = offset
+    update()
   }
 
   return {
