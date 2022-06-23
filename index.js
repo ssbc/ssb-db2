8482260ad3fb886ec88bff95b8ab67baef96fabf
Andre Staltz <andre@staltz.com>
2022-06-23 14:28:43 +0300

    remove commented code

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ module.exports = [
   require('./core'),
   require('ssb-classic'),
   require('ssb-box'),
-  // require('./encryption-formats/box2'),
   require('./compat/publish'),
   require('./compat/post'),
   require('./migrate'),
