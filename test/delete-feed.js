9e7136f47615bbd3951b0d0abbafcaf11dfbec8f
Andre Staltz <andre@staltz.com>
2022-06-29 11:59:01 +0300

    test that live fullMentions is silent during reindexing

--- test/delete-feed.js
+++ test/delete-feed.js
@@ -13,7 +13,8 @@ const SecretStack = require('secret-stack')
 const caps = require('ssb-caps')
 const generateFixture = require('ssb-fixtures')
 const fs = require('fs')
-const { where, author, toPromise } = require('../operators')
+const { where, live, author, toPromise, toPullStream } = require('../operators')
+const mentions = require('../operators/full-mentions')
 
 const dir = '/tmp/ssb-db2-delete-feed-compact'
 
@@ -43,6 +44,7 @@ test('delete a feed and then compact', async (t) => {
   const keys = ssbKeys.loadOrCreateSync(path.join(dir, 'secret'))
   const sbot = SecretStack({ appKey: caps.shs })
     .use(require('../'))
+    .use(require('../full-mentions'))
     .call(null, {
       keys,
       path: dir,
@@ -60,6 +62,22 @@ test('delete a feed and then compact', async (t) => {
       })
     )
   })
+
+  let gotSpecialMention = false
+  pull(
+    sbot.db.query(where(mentions(sbot.id)), live(), toPullStream()),
+    pull.drain((msg) => {
+      if (msg.value.content.special) {
+        if (gotSpecialMention) {
+          t.fail('got more than one special mention')
+        } else {
+          gotSpecialMention = true
+        }
+      } else {
+        t.fail('should not get any live mentions during reindexing')
+      }
+    })
+  )
 
   const badPerson = '@a5URnr0bEtVFmNxQu/JwQ5JsfEl+HAPz5ethL+I1CCA=.ed25519'
   t.notEqual(sbot.id, badPerson, 'I am not the bad person')
@@ -95,6 +113,17 @@ test('delete a feed and then compact', async (t) => {
 
   const msgsBad2 = await sbot.db.query(where(author(badPerson)), toPromise())
   t.equals(msgsBad2.length, 0, 'zero messages by bad person')
+
+  await pify(sbot.db.publishAs)(ssbKeys.generate(), {
+    type: 'post',
+    text: 'Hi',
+    special: 'yes',
+    mentions: [{ link: sbot.id }],
+  })
+  t.pass('published new mention')
+
+  await pify(setTimeout)(1000)
+  t.true(gotSpecialMention, 'got special mention')
 
   await pify(sbot.close)(true)
   t.end()
