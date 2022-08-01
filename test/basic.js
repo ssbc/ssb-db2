c0b8407429bf640098ca335d31c279d5a46636c2
Andre Staltz <andre@staltz.com>
2022-08-01 22:04:50 +0300

    add logStats() API

--- test/basic.js
+++ test/basic.js
@@ -167,6 +167,17 @@ test('delete single msg', (t) => {
   })
 })
 
+test('logStats()', (t) => {
+  db.logStats((err, stats) => {
+    t.error(err, 'no err')
+    t.equals(stats.totalCount, 34)
+    t.equals(stats.totalBytes, 13312)
+    t.equals(stats.deletedCount, 1)
+    t.equals(stats.deletedBytes, 405)
+    t.end()
+  })
+})
+
 test('deleteFeed', (t) => {
   const post = { type: 'post', text: 'Testing!' }
   const post2 = { type: 'post', text: 'Testing 2!' }
