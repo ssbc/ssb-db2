a3728ca7282269d8226ab7875d7f1a88c8530d22
mixmix <mix@protozoa.nz>
2022-09-13 22:15:49 +1200

    Deprecation notes + log for about-self

--- about-self.js
+++ about-self.js
@@ -2,8 +2,12 @@
 //
 // SPDX-License-Identifier: LGPL-3.0-only
 
+// DEPRECATED
+// please use https://github.com/ssbc/ssb-about-self
 const AboutSelfIndex = require('./indexes/about-self')
 
 exports.init = function (sbot, config) {
+  console.log('ssb-db2/about-self is deprecated, please use ssb-about-self')
+
   sbot.db.registerIndex(AboutSelfIndex)
 }
--- indexes/about-self.js
+++ indexes/about-self.js
@@ -2,4 +2,6 @@
 //
 // SPDX-License-Identifier: LGPL-3.0-only
 
+// DEPRECATED
+// please use https://github.com/ssbc/ssb-about-self
 module.exports = require('ssb-about-self/plugin.js')
