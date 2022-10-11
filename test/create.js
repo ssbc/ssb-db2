4dae8deef08cfd196aa658dbfc04ad28cee4baf9
Jacob Karlsson <jacob.karlsson95@gmail.com>
2022-10-11 14:35:13 +0200

    Update error message to match for

--- test/create.js
+++ test/create.js
@@ -60,30 +60,29 @@ test('create() classic', (t) => {
 test('create() classic "too big"', (t) => {
   const content = {
     type: 'post',
-    text: Array(10000).fill('!').join('')
+    text: Array(10000).fill('!').join(''),
   }
-  db.create(
-    { feedFormat: 'classic', content, },
-    (err, msg) => {
+  db.create({ feedFormat: 'classic', content }, (err, msg) => {
+    const comment = 'should error when content > 8kb'
+    if (!err) t.fail(comment)
+    else t.match(err.message, /create() failed/, comment)
+
+    const content = {
+      type: 'post',
+      text: Array(8192 - 114)
+        .fill('!')
+        .join(''),
+      recps: [sbot.id],
+    }
+
+    db.create({ feedFormat: 'classic', content }, (err) => {
       const comment = 'should error when content > 8kb'
       if (!err) t.fail(comment)
-      else t.match(err.message, /content must be at most 8192/, comment)
+      else t.match(err.message, /create() failed/, comment)
 
-      const content = {
-        type: 'post',
-        text: Array(8192 - 114).fill('!').join(''),
-        recps: [sbot.id]
-      }
-
-      db.create({ feedFormat: 'classic', content }, err => {
-        const comment = 'should error when content > 8kb'
-        if (!err) t.fail(comment)
-        else t.match(err.message, /content must be at most 8192/, comment)
-
-        t.end()
-      })
-    }
-  )
+      t.end()
+    })
+  })
 })
 
 test('create() classic box', (t) => {
