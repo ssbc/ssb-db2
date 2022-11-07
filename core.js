c1b54d1aaae5607152a3b94908ccec8d9613ae94
Anders Rune Jensen <arj03@protonmail.ch>
2022-11-07 22:42:29 +0100

    Simplify deleteFeed

--- core.js
+++ core.js
@@ -757,24 +757,15 @@ exports.init = function (sbot, config) {
       () => {
         if (!state.has(feedId)) return cb()
 
-        self.query(
-          where(author(feedId)),
-          asOffsets(),
-          toCallback((err, offsets) => {
+        pull(
+          self.query(where(author(feedId)), asOffsets(), toPullStream()),
+          pull.asyncMap(log.del),
+          pull.collect((err) => {
             // prettier-ignore
-            if (err) return cb(clarify(err, 'deleteFeed() failed to query jitdb for ' + feedId))
+            if (err) return cb(clarify(err, 'deleteFeed() failed for feed ' + feedId))
 
-            push(
-              push.values(offsets),
-              push.asyncMap(log.del),
-              push.collect((err) => {
-                // prettier-ignore
-                if (err) return cb(clarify(err, 'deleteFeed() failed for feed ' + feedId))
-
-                state.delete(feedId)
-                indexes.base.removeFeedFromLatest(feedId, cb)
-              })
-            )
+            state.delete(feedId)
+            indexes.base.removeFeedFromLatest(feedId, cb)
           })
         )
       }
